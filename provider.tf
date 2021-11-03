37b28337d0639f1faddd586305a97b58e460d3e7
Gompei <shoueigonpei6028@gmail.com>
2021-11-03 19:18:15 +0900

    デフォルトタグ修正

--- provider.tf
+++ provider.tf
@@ -13,9 +13,9 @@ terraform {
 provider "aws" {
   region = var.region
 
-//  default_tags {
-//    tags = {
-//      project = var.project_name
-//    }
-//  }
+  default_tags {
+    tags = {
+      project = var.project_name
+    }
+  }
 }
