24624cd9400ad3e75124581fb5e45ac09d9a58f3
Gompei <shoueigonpei6028@gmail.com>
2021-11-04 09:31:11 +0900

    フロント・api側のサンプルコード追加

---
+++ example_code/lambda/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    map[string]string{"Content-Type": "application/json; charset=utf-8"},
+		Body:       fmt.Sprintln("Hello World!!"),
+	}, nil
+}
