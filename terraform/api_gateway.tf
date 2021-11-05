648d91e0fb6748ee5d7b9943f3b04f39beb4d79a
Gompei <shoueigonpei6028@gmail.com>
2021-11-05 09:12:43 +0900

    POSTに修正
    https://aws.amazon.com/jp/premiumsupport/knowledge-center/api-gateway-lambda-template-invoke-error/

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -75,7 +75,7 @@ resource "aws_api_gateway_integration" "api" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.api.id
   http_method             = aws_api_gateway_method.api.http_method
-  integration_http_method = "ANY"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.api.invoke_arn
 }
