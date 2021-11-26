56600e5de721282a542de58fdbcbd04a1550564b
Gompei <shoueigonpei6028@gmail.com>
2021-11-26 18:18:57 +0900

    apiゲートウェイキャッシュ設定

--- terraform/cloud_front.tf
+++ terraform/cloud_front.tf
@@ -1,6 +1,39 @@
 resource "aws_cloudfront_distribution" "distribution" {
   aliases = [var.root_domain]
 
+  // api gateway 設定
+  origin {
+    domain_name = replace(aws_api_gateway_deployment.api.invoke_url, "/^https?://([^/]*).*/", "$1")
+    origin_id   = "my-site-api"
+    origin_path = "/api"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  ordered_cache_behavior {
+    path_pattern           = "/api/*"
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "my-site-api"
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600  # 1 hour
+    max_ttl                = 86400 # 24 hours
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  // S3 設定
   origin {
     domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
     origin_id   = aws_s3_bucket.bucket.id
@@ -39,9 +72,9 @@ resource "aws_cloudfront_distribution" "distribution" {
     max_ttl                = 86400 # 24 hours
 
     forwarded_values {
-      query_string = false
+      query_string = true
       cookies {
-        forward = "none"
+        forward = "all"
       }
     }
 
