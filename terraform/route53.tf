0c0b813b9e1d41ec0275cb43511bd8abca96dd7f
Gompei <shoueigonpei6028@gmail.com>
2021-11-26 18:25:05 +0900

    api gateway用レコード削除

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -9,15 +9,3 @@ resource "aws_route53_record" "cloudfront_alias" {
     zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
   }
 }
-
-resource "aws_route53_record" "api_gateway_alias" {
-  zone_id = var.host_zone_id
-  name    = aws_api_gateway_domain_name.api.domain_name
-  type    = "A"
-
-  alias {
-    evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.api.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.api.regional_zone_id
-  }
-}
