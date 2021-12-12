fd5270545d95ee344e2e30834a3220405c041fbe
Gompei <shoueigonpei6028@gmail.com>
2021-12-12 16:09:28 +0900

    dynamoテーブル追加

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -10,3 +10,16 @@ resource "aws_dynamodb_table" "dynamodb_table" {
     type = "N"
   }
 }
+
+resource "aws_dynamodb_table" "dynamodb_count_table" {
+  name           = "article_count_table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "name"
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+}
