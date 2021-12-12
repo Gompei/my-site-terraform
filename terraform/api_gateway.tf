142d01c447f620fbd29031841af853ddbfa8e331
Gompei <shoueigonpei6028@gmail.com>
2021-12-12 21:44:03 +0900

    cors設定追加

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -232,11 +232,33 @@ resource "aws_api_gateway_method" "options" {
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
+resource "aws_api_gateway_method" "options_first_path" {
+  for_each      = toset(local.first_path)
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway_rest_api.id
+  resource_id   = aws_api_gateway_resource.first_path[each.value].id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
 resource "aws_api_gateway_method_response" "options" {
   for_each    = toset(local.root_path)
   rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
   resource_id = aws_api_gateway_resource.root_path[each.value].id
   http_method = aws_api_gateway_method.options[each.value].http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+resource "aws_api_gateway_method_response" "options_first_path" {
+  for_each    = toset(local.first_path)
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
+  resource_id = aws_api_gateway_resource.first_path[each.value].id
+  http_method = aws_api_gateway_method.options_first_path[each.value].http_method
   status_code = "200"
   response_models = {
     "application/json" = "Empty"
@@ -260,11 +282,39 @@ resource "aws_api_gateway_integration_response" "options" {
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
+resource "aws_api_gateway_integration_response" "options_first_path" {
+  for_each    = toset(local.first_path)
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
+  resource_id = aws_api_gateway_resource.first_path[each.value].id
+  http_method = aws_api_gateway_method.options_first_path[each.value].http_method
+  status_code = aws_api_gateway_method_response.options_first_path[each.value].status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
 resource "aws_api_gateway_integration" "options_mock" {
   for_each    = toset(local.root_path)
   rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
   resource_id = aws_api_gateway_resource.root_path[each.value].id
   http_method = aws_api_gateway_method.options[each.value].http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = <<EOF
+{
+  "statusCode": 200
+}
+EOF
+  }
+}
+resource "aws_api_gateway_integration" "options_mock_first_path" {
+  for_each    = toset(local.first_path)
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
+  resource_id = aws_api_gateway_resource.first_path[each.value].id
+  http_method = aws_api_gateway_method.options_first_path[each.value].http_method
   type        = "MOCK"
 
   request_templates = {
